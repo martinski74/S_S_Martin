51e0e049fbabd1eaa49bf6c9e14df10430b0471d
martinski74 <martin.dobr@abv.bg>
2018-06-12 09:32:55 +0300

    update

--- js/carousel.js
+++ js/carousel.js
@@ -1,18 +1,17 @@
-var myIndex = 0;
-        
-        function carousel() {
-        
-            var slide = document.getElementsByClassName("mySlides");
 
-            for (var i = 0; i < x.length; i++) {
-               slide[i].style.display = "none";  
-            }
-            myIndex++;
-
-            if (myIndex > slide.length) {
-                myIndex = 1;
+      var myIndex = 0;
+      
+      function carousel() {
+          var i;
+          var element = document.getElementsByClassName("mySlides");
+          for (i = 0; i < element.length; i++) {
+            element[i].style.display = "none";  
+          }
+          myIndex++;
+          if (myIndex > element.length) {
+              myIndex = 1
             }    
-            slide[myIndex-1].style.display = "block";  
-            setTimeout(carousel, 7000);    
-        }
-        carousel();
+          element[myIndex-1].style.display = "block";  
+          setTimeout(carousel, 7000);    
+      }
+      carousel();
