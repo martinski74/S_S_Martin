482b7387e16140123800ef576ac8de6a7fae933f
martinski74 <martin.dobr@abv.bg>
2018-06-15 14:38:25 +0300

    css fixing

--- js/carousel.js
+++ js/carousel.js
@@ -1,16 +1,17 @@
-      var myIndex = 0;
 
-      function carousel() {
+var myIndex = 0;
+function carousel() {
 
-          var element = document.getElementsByClassName("mySlides");
-          for (var i = 0; i < element.length; i++) {
-            element[i].style.display = "none";  
-          }
-          myIndex++;
-          if (myIndex > element.length) {
-              myIndex = 1
-            }    
-          element[myIndex-1].style.display = "block";  
-          setTimeout(carousel, 7000);    
-      }
-      carousel();
+  let element = document.getElementsByClassName("mySlides");
+  for (let i = 0; i < element.length; i++) {
+    element[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > element.length) {
+    myIndex = 1
+  }
+  element[myIndex - 1].style.display = "block";
+  setTimeout(carousel, 7000);
+
+}
+carousel();
