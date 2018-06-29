10d99c9fdce3f65cb214f79a7865a4c2e089a640
martinski74 <martin.dobr@abv.bg>
2018-06-29 23:01:15 +0300

    visit counter added

--- js/counter.js
+++ js/counter.js
@@ -1,11 +1,15 @@
-function counter () {
+
+function counter() {
+
     if (localStorage.pagecount) {
         localStorage.pagecount = Number(localStorage.pagecount) + 1;
     }
     else {
         localStorage.pagecount = 1;
     }
-   document.getElementById('visits')
-    .innerHTML="Visits: " + localStorage.pagecount + " time(s).";
+
+    document.getElementById('visits')
+        .innerHTML = "VISITORS: " + localStorage.pagecount;
+
 }
 counter();
