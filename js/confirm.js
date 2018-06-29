8cdc69fb6970ac767950f9ae36b7ade97abe0bff
martinski74 <martin.dobr@abv.bg>
2018-06-29 10:43:38 +0300

    update

--- js/confirm.js
+++ js/confirm.js
@@ -4,7 +4,7 @@ $('#confirm').click(function (ev) {
 
     let result = $('<div>').addClass('notification');
     result.appendTo('.flex');
-    let message = $('<h2>').text('Your reservation is successfull!');
+    let message = $('<h2>').text('Your reservation is accepted successfully!');
     message.css('margin-top', '75px');
     message.css('text-align', 'center');
     message.appendTo(result);
