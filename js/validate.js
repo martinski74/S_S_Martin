61e42d3d45390cb9c1c6301ea63ca0d3c35e6764
martinski74 <martin.dobr@abv.bg>
2018-07-05 15:53:34 +0300

    validation added

---
+++ js/validate.js
@@ -0,0 +1,35 @@
+function validate() {
+
+    let name = $('#formGroupName').val();
+    let email = $('#formGroupEmail').val();
+    let phone = $('#formGroupPhone').val();
+    let text = $('#message').val();
+
+    if (name != '') {
+        $('.name-message').hide();
+    }
+    if (name == '') {
+        $('.name-message').text('This field is reqired!').css('color', 'red');
+    }
+    if (email != '') {
+        $('.email-message').hide();
+    }
+    if (email == '') {
+        $('.email-message').text('This field is reqired!').css('color', 'red');
+    }
+    if (phone != '') {
+        $('.phone-message').hide();
+    }
+    if (phone == '') {
+        $('.phone-message').text('This field is reqired!').css('color', 'red');
+    }
+    if (text != '') {
+        $('.text-message').hide();
+    }
+    if (text == '') {
+        $('.text-message').text('This field is reqired!').css('color', 'red');
+    }
+
+    
+
+}
